a7f6c2fa9a8eeb1df17c89c47977062d9eee09a2
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-07 13:35:48 -0700

    commit tree ef7dfd3437fc89436129211a4a2eb009a29e5baa

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,42 @@
-# your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        limit && @limit = limit
+    end
+
+    def push(value)
+        if @stack.size == @limit
+            raise Exception.new "Stack Overflow"
+        else
+        @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def search(val)
+        index = @stack.find_index(val)
+        index ? self.size - index - 1 : -1
+    end
+
+
+end
